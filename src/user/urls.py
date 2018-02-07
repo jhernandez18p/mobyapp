5b9932ff516a762cf77876c175d6e3df60543d63
Josmer Hernandez <jhernandez.18p@gmail.com>
2018-02-07 16:18:50 -0500

    updates

--- src/user/urls.py
+++ src/user/urls.py
@@ -3,8 +3,8 @@ from .views import (Login,Logout,Register, activate)
 
 app_name = 'auth'
 urlpatterns = [
-    path('/login', Login.as_view(), name='login'),
-    path('/logout', Logout.as_view(), name='logout'),
-    path('/register', Register.as_view(), name='register'),
+    path('/login/', Login.as_view(), name='login'),
+    path('/logout/', Logout.as_view(), name='logout'),
+    path('/register/', Register.as_view(), name='register'),
     re_path('/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})$', activate, name='activate'),
 ]
