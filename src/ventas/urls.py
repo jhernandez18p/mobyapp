40c07def3558d42f724b01f8836b38c80ba19272
Josmer Hernandez <jhernandez.18p@gmail.com>
2018-02-16 08:45:40 -0500

    updates

--- src/ventas/urls.py
+++ src/ventas/urls.py
@@ -13,7 +13,7 @@ app_name = 'sales'
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('/todos', Products_List.as_view(), name='all'),
-    path('/detalle/<slug:slug>', Products_Detail, name='product_detail'),
+    path('/detalle/<slug:slug>', Products_Detail.as_view(), name='product_detail'),
     
     path('/departamentos', Departments.as_view(), name='departments'),
     path('/departamento/<str:slug>', DepartmentDetail.as_view(), name='department_detail'),
