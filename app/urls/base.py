c1ca54498c234bae6a48915cfd5ee4e6ceab7217
Josmer Hernandez <jhernandez.18p@gmail.com>
2018-02-27 08:54:35 -0500

    errors

--- app/urls/base.py
+++ app/urls/base.py
@@ -7,12 +7,11 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.conf.urls import handler404,handler500,handler403,handler400
 
 from src.base import views as base_views
 from rest_framework import routers
 from app.urls import rest
-
-from app.urls import error
 
 router = routers.DefaultRouter()
 router.register(r'users', rest.UserViewSet)
@@ -22,10 +21,6 @@ sitemaps = {
     'flatpages': FlatPageSitemap,
 }
 
-handler404 = error.page_not_found_view
-handler500 = error.error_view
-handler403 = error.permission_denied_view
-handler400 = error.bad_request_view
 
 urlpatterns = [
     path('', include('src.base.urls', namespace='front')),
@@ -55,3 +50,8 @@ if settings.DEBUG:
 
 admin.site.site_title = 'Dev2tech CMS'
 admin.site.site_header = 'CMS Moby Supply'
+
+handler404 = 'app.urls.error.page_not_found_view'
+handler500 = 'app.urls.error.error_view'
+handler403 = 'app.urls.error.permission_denied_view'
+handler400 = 'app.urls.error.bad_request_view'
