b5aa802932cf910105456e31ea800d87779819ab
Josmer Hernandez <jhernandez.18p@gmail.com>
2018-03-09 15:45:09 -0500

    update

--- src/services/forms.py
+++ src/services/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ServiceForm(forms.Form):
     name = forms.CharField(max_length=144, label='Nombre',)
     email = forms.CharField(max_length=144, label='Correo electronico',)
-    subject = forms.CharField(max_length=144, label='Asulto',)
+    subject = forms.CharField(max_length=144, label='Asunto',)
     message = forms.CharField(max_length=744, label='Mensaje',)
